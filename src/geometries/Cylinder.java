f347d50ff9bb8980477ed42d01212e2a2f810715
minipNaf <mainvilner@gmail.com>
2025-04-24 15:20:35 +0300

    cylinder tests + implementation

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -2,9 +2,12 @@
 package geometries;
 import primitives.*;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import static primitives.Util.isZero;
+import java.util.List;
+import static java.lang.System.out;
+
+import static primitives.Util.*;
 
 /**
  * Represents a finite cylinder in 3D space.
@@ -69,6 +72,97 @@ public class Cylinder extends Tube{
 
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        Point rayHead = ray.getHead();
+        Plane base1 = new Plane(axis.getHead(), axis.getDirection());
+        Plane base2 = new Plane(axis.getHead().add(axis.getDirection().scale(height)), axis.getDirection());
+        List<Point> intersections = new ArrayList<>();
+        List<Point> temp; //to check if a list is null beforehand
+        temp = super.findIntersections(ray);
+        if(temp != null) intersections.addAll(temp);
+        temp = base1.findIntersections(ray);
+        if(temp != null) intersections.addAll(temp);
+        temp = base2.findIntersections(ray);
+        if(temp != null) intersections.addAll(temp);
+        double t, s;
+        Vector v;
+        Point p;
+        int size = intersections.size();
+        for(int i = 0; i < size; i++) {
+
+            // if t < 0, the point is below base1, if t > height, the point is above base2
+        // t represents the difference in height between base1 and the first intersection point
+            p = intersections.get(i);
+            if(p.equals(axis.getHead())) {
+                t = 0;
+                s = 0;
+            }
+            else{
+                v = p.subtract(axis.getHead());
+                t = axis.getDirection().dotProduct(v);
+                s = Math.sqrt(alignZero(v.lengthSquared() - t * t));
+            }
+            if (alignZero(t) < 0 || alignZero(t - height) > 0 || alignZero(s - radius) > 0) {
+                intersections.remove(p);
+                i--;
+                size--;
+            }
+
+        }
+        if(intersections.isEmpty()) {
+            return null;
+        }
+        if(intersections.getFirst().equals(intersections.getLast())) return List.of(intersections.getFirst());
+        if(intersections.getFirst().distanceSquared(rayHead) > intersections.getLast().distanceSquared(rayHead)) {
+            Point temp1 = intersections.getFirst();
+            intersections.set(0, intersections.getLast());
+            intersections.set(1, temp1);
+        }
+        return intersections;
+//        if(tubeIntersections == null) {
+//            return null;
+//        }
+//        if(tubeIntersections.size() == 1) {//ray starts inside the tube that contains the cylinder
+//            Point p = tubeIntersections.getFirst();
+//            // Check if the point is on the cylinder's surface
+//            double t = axis.getDirection().dotProduct(p.subtract(axis.getHead()));
+//            if (Util.alignZero(t) < 0 || alignZero(t - height) > 0) { //intesection point is outside the cylinder
+//                double u = rayHead.subtract(axis.getHead()).dotProduct(axis.getDirection());
+//                if(Util.alignZero(u) < 0 || alignZero(u - height) > 0) //ray starts outside the cylinder
+//                    return null;
+//
+//                if(Util.alignZero(ray.getDirection().dotProduct(axis.getDirection()))>0) {
+//                    return base2.findIntersections(ray); // The point is on the top base
+//                }
+//                else {
+//                    return base1.findIntersections(ray); // The point is on the bottom base
+//                }
+//
+//            }
+//            return List.of(p); // The point is on the cylinder's surface
+//        }
+//        // if t1 < 0, the point is below base1, if t1 > height, the point is above base2
+//        // t1 represents the difference in height between base1 and the first intersection point
+//        double t1 = axis.getDirection().dotProduct(tubeIntersections.getFirst().subtract(axis.getHead()));
+//        // if t2 < 0, the point is below base1, if t2 > height, the point is above base2
+//        // t2 represents the difference in height between base1 and the second intersection point
+//        double t2 = axis.getDirection().dotProduct(tubeIntersections.getLast().subtract(axis.getHead()));
+//        // check if both points are below base1 or above base2
+//        if(Util.alignZero(t1) < 0 && Util.alignZero(t2) < 0 ||
+//                Util.alignZero(t1 - height) > 0 && Util.alignZero(t2 - height) > 0) {
+//            return null;
+//        }
+//        // check if one point is below base1 and the other is above base2
+//        // if it is that way, the ray intersects the cylinder in both bases
+//        if(Util.alignZero(t1) < 0 && Util.alignZero(t2 - height) > 0) {
+//            return List.of(base1.findIntersections(ray).getFirst(), base2.findIntersections(ray).getFirst());
+//        }
+//        // check if one point is below base1 and the other is above base2
+//        // if it is that way, the ray intersects the cylinder in both bases
+//        if(Util.alignZero(t2) < 0 && Util.alignZero(t1 - height) > 0) {
+//            return List.of(base2.findIntersections(ray).getFirst(), base1.findIntersections(ray).getFirst());
+//        }
+//
+//        return null;
+
     }
 }
--- unittests/geometries/CylinderTest.java
+++ unittests/geometries/CylinderTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -16,6 +16,7 @@ class CylinderTest {
      */
     Cylinder cylinder = new Cylinder(3, new Ray(new Vector(0,0,1), new Point(0,0, 2)), 2);
     Point p1 = new Point(-0.25,-2.99,2.8);
+    double xy = Math.sqrt(2)/2*3;
 
     /**
      * Test method for {@link geometries.Cylinder#Cylinder(double, Ray, double)}
@@ -60,9 +61,53 @@ class CylinderTest {
                 "ERROR: wrong normal for base edge");
 
     }
+    /**
+     * Test method for {@link geometries.Cylinder#findIntersections(Ray)}
+     * tests the intersections of the cylinder
+     */
 
     @Test
     void testFindIntersections() {
+        // ============ Equivalence Partitions Tests ==============
+        //test 01: ray intersects the round surface and then the base(2 points)
+        assertEquals(List.of(new Point(xy,xy,3), new Point(1,1,4)),
+                cylinder.findIntersections(new Ray(new Vector(1-xy,1-xy,1), new Point(2*xy-1,2*xy-1,2))),
+                "ERROR: wrong intersection points");
+        //test 02: ray intersects the base and then the round surface(2 points)
+        assertEquals(List.of(new Point(1,1,4), new Point(xy,xy,3)),
+                cylinder.findIntersections(new Ray(new Vector(xy-1,xy-1,-1), new Point(2-xy,2-xy,5))),
+                "ERROR: wrong intersection points");
+        //test 03: ray intersects the round surface twice(2 points)
+        assertEquals(List.of(new Point(xy,xy,3), new Point(-xy,-xy,3)),
+                cylinder.findIntersections(new Ray(new Vector(-1,-1,0), new Point(5,5,3))),
+                "ERROR: wrong intersection points");
+        //test 04: ray intersects the bases twice(2 points)
+        assertEquals(List.of(new Point(0,1,4), new Point(1,0,2)),
+                cylinder.findIntersections(new Ray(new Vector(1,-1,-2), new Point(-1,2,6))),
+                "ERROR: wrong intersection points");
+        //test 05: ray doesn't intersect the cylinder but its line does(0 points)
+        assertNull(cylinder.findIntersections(new Ray(new Vector(1,1,0), new Point(5,5,3))),
+                "ERROR: there must be 0 points(null)");
+        //test 06: ray starts inside the cylinder and goes out through the base(1 point)
+        assertEquals(List.of(new Point(1,0,2)),
+                cylinder.findIntersections(new Ray(new Vector(1,-1,-1), new Point(0,1,3))),
+                "ERROR: wrong intersection points");
+        // =========== Boundary Values Tests =====================
+        //test 01: ray intersects the perpendicular line of the cylinder(1 point)
+        assertEquals(List.of(new Point(0,3,4)),
+                cylinder.findIntersections(new Ray(new Vector(0,1,1), new Point(0,2,3))),
+                "ERROR: wrong intersection points");
+        //test 02: ray intersects the cylinder at the center of the base(1 point)
+        assertEquals(List.of(new Point(0,0,4)),
+                cylinder.findIntersections(new Ray(new Vector(0,1,1), new Point(0,-1,3))),
+                "ERROR: wrong intersection points");
+        //test 03: ray goes the same direction as the cylinder's axis and intersects the bases(2 points)
+        assertEquals(List.of(new Point(0,0,2), new Point(0,0,4)),
+                cylinder.findIntersections(new Ray(new Vector(0,0,1), new Point(0,0,-1))),
+                "ERROR: wrong intersection points");
+        //test 04: ray starts directly at the base(0 point)
+        assertNull(cylinder.findIntersections(new Ray(new Vector(0,1,1), new Point(0,1,4))),
+                "ERROR: there must be 0 points(null)");
 
     }
 }
