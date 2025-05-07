7e494d721afba743fb9ab1b0d695a94cd6367495
YehudaFeldman <yehudafeldman30@gmail.com>
2025-05-07 16:01:16 +0300

    wrote test in ImageWriterTest

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,13 +1,35 @@
 package renderer;
 
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ImageWriterTest {
     private static final String TEST_IMAGE_NAME = "testImage";
-    private static final int IMAGE_WIDTH = 800;
-    private static final int IMAGE_HEIGHT = 500;
+    private static final int IMAGE_WIDTH = 801;
+    private static final int IMAGE_HEIGHT = 501;
 
-    public static void main(String[] args) {
-        ImageWriter imageWriter = new ImageWriter(IMAGE_WIDTH, IMAGE_HEIGHT);
-        imageWriter.writeToImage();
-        System.out.println("Image written successfully.");
+    @Test
+    public void testImageWriter() {
+        assertDoesNotThrow(()-> {
+            ImageWriter imageWriter = new ImageWriter(IMAGE_WIDTH, IMAGE_HEIGHT);
+            Color yellow = new Color(255, 255, 0);
+            Color red = new Color(255, 0, 0);
+
+            for (int i = 0; i < IMAGE_WIDTH; i++) {
+                for (int j = 0; j < IMAGE_HEIGHT; j++) {
+                    if (i%50 == 0 || j%50 == 0) {
+                        imageWriter.writePixel(i, j, red);
+                    } else {
+                        imageWriter.writePixel(i, j, yellow);
+                    }
+                }
+            }
+            imageWriter.writeToImage(TEST_IMAGE_NAME);
+                }, "ERROR: failed to create an image");
+
     }
+
+
 }
